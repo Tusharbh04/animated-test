a1f2c159931a6f98434e6714b2441263fab6772d
Tushar Bhaladhare <tushar23rd@gmail.com>
2025-02-06 13:15:55 +0530

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import TicktokIcon from "../assets/icons/tiktok.svg";
+import InstaIcon from "../assets/icons/insta.svg";
+import XSocial from "../assets/icons/x-social.svg";
+import YoutubeIcon from "../assets/icons/youtube.svg";
+export const Footer = () => {
+  return (
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+      <div className="container">
+        <div className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+            <div className="text-center">© 2025 husk, Inc. All rights reserved</div>
+            <ul className="flex justify-center gap-2.5">
+              <li><XSocial /></li>
+              <li><InstaIcon/></li>
+              <li><TicktokIcon/></li>
+              <li><YoutubeIcon/></li>
+            </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
